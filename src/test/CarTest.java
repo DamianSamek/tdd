1f6b3543cd6c42766fd57d627569ae22e38b506f
Damian Samek <damiansamek95@gmail.com>
2021-03-20 17:26:26 +0100

    car object instantation assertion

--- src/test/CarTest.java
+++ src/test/CarTest.java
@@ -1,5 +1,14 @@
 package test;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class CarTest {
 
+	@Test
+	public void canInstantiateCarObject() {
+		Car car = new Car();
+		assertNotNull(car);
+	}
 }

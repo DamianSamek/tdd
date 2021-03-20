8535c13789d4a02df49e15b434322d90c36a728f
Damian Samek <damiansamek95@gmail.com>
2021-03-20 17:48:51 +0100

    testThatConstructorParametersAreExposed

--- src/test/CarTest.java
+++ src/test/CarTest.java
@@ -42,4 +42,13 @@ public class CarTest {
 	public void testCarClassHasConstructorContainingFourParameters() {
 		Car car = new Car("Opel", "black", 70.0, 10.0);
 	}
+	
+	@Test
+	public void testThatConstructorParametersAreExposed() {
+		Car car = new Car("Opel", "black", 70.0, 10.0);
+		car.getColor();
+		car.getBrand();
+		car.getFuelConsumption();
+		car.getTankCapacity();
+	}
 }

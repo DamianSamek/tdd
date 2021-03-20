60f0c07d0720e33827c99f774cb0bc31c494839b
Damian Samek <damiansamek95@gmail.com>
2021-03-20 17:46:24 +0100

    constructor with 4 parameters added

--- src/impl/Car.java
+++ src/impl/Car.java
@@ -3,11 +3,15 @@ package impl;
 public class Car {
 
 	public String color;
-	
+
 	public String brand;
-	
+
 	public Double tankCapacity;
-	
+
 	public Double fuelConsumption;
-	
+
+	public Car(String brand, String color, Double tankCapacity, Double fuelConsumption) {
+
+	}
+
 }

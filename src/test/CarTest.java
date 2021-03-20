ddf4f4d196220552bbe30734ed0fed8768f3860b
Damian Samek <damiansamek95@gmail.com>
2021-03-20 18:01:34 +0100

    testThatConstructorWorksProperly

--- src/test/CarTest.java
+++ src/test/CarTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 
 import impl.Car;
@@ -18,5 +20,14 @@ public class CarTest {
 		car.getBrand();
 		car.getFuelConsumption();
 		car.getTankCapacity();
+	}
+	
+	@Test
+	public void testThatConstructorWorksProperly() {
+		Car car = new Car("Opel", "black", 70.0, 10.0);
+		assertEquals("Opel", car.getBrand());
+		assertEquals("black", car.getColor());
+		assertEquals(70.0, car.getTankCapacity());
+		assertEquals(10.0, car.getFuelConsumption());
 	}
 }
